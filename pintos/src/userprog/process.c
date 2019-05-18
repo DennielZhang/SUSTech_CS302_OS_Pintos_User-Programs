606fa96ac5860ca7260b9cb28f1e8d29bd332366
DennielZhang <849207068@qq.com>
2019-05-18 23:50:23 +0800

    modified thread.c added

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -129,7 +129,7 @@ process_exit (void)
   if (exit_code == INIT_EXIT_STAT)
     exit_process(-1);
   printf("%s: exit(%d)\n", cur->name, exit_code);
-  if(lock->holder == thread_current ()){
+  if(filesys_lock->holder == thread_current ()){
     lock_release(&filesys_lock);
   }
   lock_acquire(&filesys_lock);
