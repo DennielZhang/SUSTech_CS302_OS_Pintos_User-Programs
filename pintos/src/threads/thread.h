8229276d820c3412255244c7c36c673e728c5c89
DennielZhang <849207068@qq.com>
2019-05-18 23:44:28 +0800

    modified thread.c

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -25,7 +25,7 @@ typedef int tid_t;
 #define PRI_MIN 0                       /* Lowest priority. */
 #define PRI_DEFAULT 31                  /* Default priority. */
 #define PRI_MAX 63                      /* Highest priority. */
-
+#define INIT_EXIT_STAT -2333 
 struct lock filesys_lock; //a global lock for filesystem
 /* A kernel thread or user process.
 
