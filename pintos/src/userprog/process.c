0a214b43aaec7ff0b12dc995290f99f22e33d5f0
DennielZhang <849207068@qq.com>
2019-05-19 10:25:17 +0800

    added more syscall

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -550,12 +550,11 @@ setup_stack (void **esp, char * file_name)
   int *argv = calloc(argc,sizeof(int));
 
   int i;
-  token = strtok_r (file_name, " ", &temp_ptr);
-  for (i=0;token!=NULL ; i++){
+  
+  for (i=0,token = strtok_r (file_name, " ", &temp_ptr);token!=NULL ; i++,token = strtok_r (NULL, " ", &temp_ptr);){
       *esp -= (strlen(token) + 1);
       memcpy(*esp,token,strlen(token) + 1);
       argv[i]=*esp;
-      token = strtok_r (NULL, " ", &temp_ptr);
   }
   free(token);
   /* make the word alignment */
